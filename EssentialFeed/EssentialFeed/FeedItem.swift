29b607b85e2c92befbef8341fe530f8eb1129941
Oleksandr Pylypenko <40546549+opylypenko@users.noreply.github.com>
2023-10-07 11:57:28 +0300

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Oleksandr Pylypenko on 07.10.2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
