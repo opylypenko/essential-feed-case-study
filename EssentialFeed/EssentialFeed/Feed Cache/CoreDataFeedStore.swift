366c26b3db1742b8d1d7badd73e0cda157bc6e18
Oleksandr Pylypenko <opilipenko@gmail.com>
2024-01-14 19:22:31 +0200

    CoreDataFeedStore.retrieve() delivers empty on empty cache

---
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeedTests
+//
+//  Created by Oleksandr Pylypenko on 14.01.2024.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+    public init() {}
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        
+    }
+}
