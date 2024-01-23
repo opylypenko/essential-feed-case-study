b77fcdaf97d0f8315a0405bd21ea1e76a0b06846
Oleksandr Pylypenko <opilipenko@gmail.com>
2024-01-24 01:57:26 +0200

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Oleksandr Pylypenko on 24.01.2024.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  struct FeedLoadingViewModel {     let isLoading- Bool }.swift
+//  EssentialFeediOS
+//
+//  Created by Oleksandr Pylypenko on 24.01.2024.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
