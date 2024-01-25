e48d855b3acfa062d3bbd614a251f562cfde91d5
Oleksandr Pylypenko <opilipenko@gmail.com>
2024-01-26 01:32:46 +0200

    Move FeedViewModel, FeedLoadingViewModel, FeedViewModel to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Oleksandr Pylypenko on 26.01.2024.
+//
+
+public struct FeedLoadingViewModel {
+    public  let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Oleksandr Pylypenko on 26.01.2024.
+//
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
