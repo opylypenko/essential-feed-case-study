b6094c6816d0237e53251f4a8df50354b17266c8
Oleksandr Pylypenko <opilipenko@gmail.com>
2024-01-25 22:57:02 +0200

    Move ErrorView to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Oleksandr Pylypenko on 25.01.2024.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
