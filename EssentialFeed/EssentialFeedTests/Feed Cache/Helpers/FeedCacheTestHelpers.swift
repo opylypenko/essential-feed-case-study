1345ae4e63f24bb4c9e935ba4dfd09c75524af6b
Oleksandr Pylypenko <opilipenko@gmail.com>
2024-01-27 01:01:41 +0200

    FeedImagePresenter displays loading image on `didStartLoadingImageData`

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -8,12 +8,12 @@
 import Foundation
 import EssentialFeed
 
-func uniqueItem() -> FeedImage {
+func uniqueImage() -> FeedImage {
     FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
 }
 
 func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
-    let items = [uniqueItem(), uniqueItem()]
+    let items = [uniqueImage(), uniqueImage()]
     let localItems = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     return (items, localItems)
 }
