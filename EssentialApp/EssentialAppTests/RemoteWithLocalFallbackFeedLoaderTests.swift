cb12b274f7979293236f24764be92e9808be7316
Oleksandr Pylypenko <opilipenko@gmail.com>
2024-02-01 21:02:01 +0200

    Create RemoteWithLocalFallbackFeedLoaderTests class

---
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialAppTests.swift
+//  EssentialAppTests
+//
+//  Created by Oleksandr Pylypenko on 28.01.2024.
+//
+
+import XCTest
+@testable import EssentialApp
+
+final class RemoteWithLocalFallbackFeedLoaderTests: XCTestCase {
+
+    
+
+}
