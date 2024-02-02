1c9c8bf7d54a8509b44009d7af1093e3656eeaad
Oleksandr Pylypenko <opilipenko@gmail.com>
2024-02-03 00:20:22 +0200

    Move FeedImageDataLoaderCacheDecorator to production

---
+++ EssentialApp/EssentialApp/FeedImageDataLoaderCacheDecorator.swift
@@ -0,0 +1,27 @@
+//
+//  FeedImageDataLoaderCacheDecorator.swift
+//  EssentialApp
+//
+//  Created by Oleksandr Pylypenko on 03.02.2024.
+//
+
+import Foundation
+import EssentialFeed
+
+public final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    private let decoratee: FeedImageDataLoader
+    private let cache: FeedImageDataCache
+    public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            completion(result.map { data in
+                self?.cache.save(data, for: url) { _ in }
+                return data
+            })
+        }
+    }
+}
