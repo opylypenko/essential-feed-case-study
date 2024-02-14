d0bb592b96ceffbc398c951f9b2d43102eeb7531
Oleksandr Pylypenko <opilipenko@gmail.com>
2024-02-14 22:11:16 +0200

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Oleksandr Pylypenko on 14.02.2024.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
